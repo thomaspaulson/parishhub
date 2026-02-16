1bb709065d9fa271824ac2631ab6848ef5e8a688
thomaspaulson <thomas.paulson@hotmail.com>
2026-02-16 14:56:50 +0530

    setup sanctum

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\DeathController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+	Route::post('register', [AuthController::class, 'register']);
+	Route::post('login', [AuthController::class, 'login']);
+	Route::post('logout', [AuthController::class, 'logout'])->middleware('auth');
+});
 
 Route::apiResource('deaths', DeathController::class);
