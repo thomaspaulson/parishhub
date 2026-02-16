15902dbc0f55c0e77fad32ed3fb44125879da0bf
thomaspaulson <thomas.paulson@hotmail.com>
2026-02-16 14:25:04 +0530

    uuid init

--- app/database/migrations/0001_01_01_000003_create_deaths_table.php
+++ app/database/migrations/0001_01_01_000003_create_deaths_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('deaths', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('full_name');
             $table->string('parent')->nullable();
             $table->string('address')->nullable();
