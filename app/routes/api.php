d5f39e389da52aabbbcdf5263d2d3856e40edadb
thomaspaulson <thomas.paulson@hotmail.com>
2026-02-17 13:27:36 +0530

    fix

--- app/routes/api.php
+++ app/routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-	Route::post('register', [AuthController::class, 'register']);
+	// Route::post('register', [AuthController::class, 'register']);
 	Route::post('login', [AuthController::class, 'login']);
 	Route::post('logout', [AuthController::class, 'logout'])->middleware('auth');
 });
